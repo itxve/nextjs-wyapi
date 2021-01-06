543ff66dbc6ecaebde21c0e68fa23f07b185010f
itxue <517400464@qq.com>
2021-01-06 17:16:11 +0800

    :ambulance: 模拟错误

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,5 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
+if (req) {
+  throw new Error("呃呃呃呃");
+  
+}
   res.status(500).json({ name: 'John Doe' })
 }
