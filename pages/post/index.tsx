9684ac47d42b3b400598c8477a22fc04d914855d
itxue <517400464@qq.com>
2021-01-06 18:30:14 +0800

    Update index.tsx

--- pages/post/index.tsx
+++ pages/post/index.tsx
@@ -1,7 +1,13 @@
 
 export default function Post() {
-  if (Date.now()) {
-    throw new Error("wwe");
-  }
   return <h1 style={{color:"#F90D0D"}}>Post</h1>
 }
+
+Post.getInitialProps = () => {
+  return {
+    error: {
+      statusCode: 401,
+      message: 'oopsie'
+    }
+  };
+};
