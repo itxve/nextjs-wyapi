389d681076e35640e0d869b6a9fa59ca09fe8b6c
itxue <517400464@qq.com>
2021-01-08 13:10:45 +0800

    更新 auth

--- config/index.tsx
+++ config/index.tsx
@@ -2,7 +2,6 @@ export default {
     github: {
         REDIRECT_URL: 'https://api.itxve.cn',
         GETACCESSTOKEN: 'https://api.itxve.cn/api/auth',
-        AUTHORIZE: `https://github.com/login/oauth/authorize?scope=user&
-        client_id=d9fa9dd33e73f65a0a61&redirect_uri=https://api.itxve.cn`
+        AUTHORIZE: `https://github.com/login/oauth/authorize?scope=user&client_id=d9fa9dd33e73f65a0a61&redirect_uri=https://api.itxve.cn`
     }
 };
