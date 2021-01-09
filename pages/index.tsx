1a237b7eb03e6f8d78b8c2468f57e89811f57eda
itxue <517400464@qq.com>
2021-01-09 23:02:46 +0800

    update INDEX

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Login from '@/pages/login';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -22,32 +23,40 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 export default function Main(props: any) {
+    const [state, setstate] = useState(true);
     useEffect(() => {
         if (!localStorage.getItem('login')) {
-            location.href = '/login';
+            setstate(false);
+            // location.href = '/login';
         }
     }, []);
 
     const classes = useStyles();
 
     return (
-        <div className={classes.root}>
-            <AppBar position="fixed">
-                <Toolbar>
-                    <IconButton
-                        edge="start"
-                        className={classes.menuButton}
-                        color="inherit"
-                        aria-label="menu"
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        News
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-            </AppBar>
-        </div>
+        <>
+            {!state ? (
+                <Login />
+            ) : (
+                <div className={classes.root}>
+                    <AppBar position="fixed">
+                        <Toolbar>
+                            <IconButton
+                                edge="start"
+                                className={classes.menuButton}
+                                color="inherit"
+                                aria-label="menu"
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Typography variant="h6" className={classes.title}>
+                                News
+                            </Typography>
+                            <Button color="inherit">Login</Button>
+                        </Toolbar>
+                    </AppBar>
+                </div>
+            )}
+        </>
     );
 }
