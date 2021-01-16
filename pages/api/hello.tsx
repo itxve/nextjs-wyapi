a47a4f91a23dbdfc28a6c7120387eb1dc43d9c60
itxue <517400464@qq.com>
2021-01-16 18:51:04 +0800

    head

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,5 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default (req: NextApiRequest, res: NextApiResponse): void => {
-    res.status(200).json({ name: 'John Doe' });
+    try {
+        fetch('https://avatars0.githubusercontent.com/u/33005087?v=4', {
+            // responseType: 'arraybuffer'
+        })
+            .then((e) => e.arrayBuffer())
+            .then((e) => {
+                var buf = new Buffer(e.byteLength);
+                var view = new Uint8Array(e);
+                for (var i = 0; i < buf.length; ++i) {
+                    buf[i] = view[i];
+                }
+                res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+                res.end(buf);
+            });
+    } catch (error) {
+        res.send(error);
+    }
 };
